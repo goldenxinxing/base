a8f329dcf0bc7d0cc5e6861ddebaea87d1f78f69
gaoxinxing <gaoxinxing@jd.com>
2018-10-26 19:03:40 +0800

    rpc test

---
+++ src/my/base/mytest/RPCTest.java
@@ -0,0 +1,30 @@
+package my.base.mytest;
+
+import my.base.Interface.HelloService;
+import my.base.Interface.impl.HelloServiceImpl;
+import my.base.registry.Server;
+import my.base.client.RPCClient;
+import my.base.registry.impl.ServiceCenter;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class RPCTest {
+
+    public static void main(String[] args) throws IOException {
+        // 模拟服务端（注册中心）
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Server serviceServer = new ServiceCenter(8088);
+                    serviceServer.register(HelloService.class, HelloServiceImpl.class);
+                    serviceServer.start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        // 模拟客户端（调用端）
+        HelloService service = RPCClient.getRemoteProxyObj(HelloService.class, new InetSocketAddress("localhost", 8088));
+        System.out.println(service.sayHi("mytest"));
+    }
+}
