d729f03a62d95f18b70f1947ae41fe3067c1d55e
gaoxinxing <gaoxinxing@megvii.com>
2019-05-13 13:46:36 +0800

    进制

---
+++ src/my/base/NumberTest.java
@@ -0,0 +1,13 @@
+package my.base;
+
+/**
+ * @Package: my.base<br>
+ * @ClassName: NumberTest.java<br>
+ * @Description: TODO
+ * @author: gaoxinxing
+ */
+public class NumberTest {
+    private int n_16 = 0x123a;
+    private int n_8 = 01237;
+    private int n_2 = 0b1010;
+}
