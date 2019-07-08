5b218320fae221ec69dc2cbcd7698cd2905d4a77
goldenxinxing <15031259256@163.com>
2019-07-08 14:41:38 +0800

    Update FuncInterDemo.java

--- src/jdk/jdk8/FuncInterDemo.java
+++ src/jdk/jdk8/FuncInterDemo.java
@@ -5,6 +5,8 @@ public interface FuncInterDemo<T> {
     default boolean compare(T a, T b) {
         return a.equals(b);
     }
+    
     String toString(T t);
+    
     boolean equals(Object obj);
 }
