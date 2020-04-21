3bace45a6a5e843a193c3eda4bb2c188b6317977
gaoxinxing <gaoxinxing@megvii.com>
2020-04-21 17:49:10 +0800

    java 位移测试

---
+++ src/jdk/jdk8/base/LeftMoveTest.java
@@ -0,0 +1,9 @@
+package jdk.jdk8.base;
+
+public class LeftMoveTest {
+    public static void main(String[] args) {
+        // 压缩大图
+        final double ratio = 64 << 10;
+        System.out.println(ratio);
+    }
+}
